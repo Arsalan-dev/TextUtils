7587eaecd6b59903484d84e233a37ce7e0d3b87c
Syed Arsalan <arsalan11772@gmail.com>
2022-03-27 23:56:12 +0200

    Updates

---
+++ precache-manifest.d98b0ceb81bf52e7f39c1fa1d806324e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffdc05d786c574966d70ec898b1f1e4f",
+    "url": "/TextUtils/index.html"
+  },
+  {
+    "revision": "ea3e789bd617c6223a46",
+    "url": "/TextUtils/static/css/main.16e18a48.chunk.css"
+  },
+  {
+    "revision": "1d84870f7c39263b1f40",
+    "url": "/TextUtils/static/js/2.81252156.chunk.js"
+  },
+  {
+    "revision": "f39df9c96013426fd10f9fc89beed3b9",
+    "url": "/TextUtils/static/js/2.81252156.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90cc4cc6e967626b5371",
+    "url": "/TextUtils/static/js/3.03e7bd60.chunk.js"
+  },
+  {
+    "revision": "ea3e789bd617c6223a46",
+    "url": "/TextUtils/static/js/main.d6d285a3.chunk.js"
+  },
+  {
+    "revision": "2d71b663cefe4b7d2f05",
+    "url": "/TextUtils/static/js/runtime-main.72cf0a3b.js"
+  }
+]);
